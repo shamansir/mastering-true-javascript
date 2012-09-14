3042d5c715f48ad4991637f8c4ff0131c5b3540c
Ulric Wilfred <shaman.sir@gmail.com>
2012-09-14 23:03:30 +0400

    custom jasmine style

--- 01-intro.spec.js
+++ 01-intro.spec.js
@@ -2,4 +2,13 @@ describe("test", function() {
     it("should pass", function() {
         expect(1+2).toBe(3);
     });
+    it("should pass", function() {
+        expect(1+2).toBe(3);
+    });
+    it("should fail", function() {
+        expect(1+7).toBe(3);
+    });
+    it("should fail", function() {
+        expect(1+7).toBe(3);
+    });
 });
