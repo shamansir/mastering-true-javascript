d96498eb5bad192e2cee52ce03d150b33992d4e3
Ulric Wilfred <shaman.sir@gmail.com>
2012-09-14 07:42:47 -0400

    additional stuff

---
+++ 02-why-js.js
@@ -0,0 +1,5 @@
+// (about libraries, current version, client side, dynamic typing flexibility; no overloading / custom operators, types vs. classes) -->
+
+// #Lazier Python programmer
+// f = lambda x: x and x * f(x - 1) or 1
+// print f(6)
